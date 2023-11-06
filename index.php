c60744f4caac3c274c82c9a19a16a22440bd27b3
Pirisinu <palazzialessandro2@gmail.com>
2023-11-06 15:43:43 +0100

    [ADD]Movies card

--- index.php
+++ index.php
@@ -3,19 +3,29 @@ class Movie {
   //Variabili d'istanza
   public $title;
   public $image;
-  public $desctiption;
-  public $language
+  public $description;
+  public $language;
   public $vote;
   public $price;
   public $discount;
 
   //Costruttore
-  public function __construct(string $_title,string $_image,string $_desctiption,int $_price){
+  public function __construct(string $_title,string $_image,string $description,int $_price){
     $this->title = $_title;
-    $this->title = $_image;
-    $this->title = $_desctiption;
-    $this->title = $_price;
+    $this->image = $_image;
+    $this->description = $description;
+    $this->price = $_price;
   }
+}
+$movies = [
+  'matrix'=> new Movie('Matrix', 'matrix.jpg', 'Matrix description', 10),
+  'barbie'=> new Movie('barbie', 'barbie.jpg', 'barbie description', 8),
+  'titanic' => new Movie('titanic', 'titanic.jpg', 'titanic description', 20),
+  'matrix2'=> new Movie('Matrix', 'matrix.jpg', 'Matrix description', 10),
+  'barbie2'=> new Movie('barbie', 'barbie.jpg', 'barbie description', 8),
+  'titanic2' => new Movie('titanic', 'titanic.jpg', 'titanic description', 20)
+];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,6 +39,17 @@ class Movie {
   <title>Php Oop-1</title>
 </head>
 <body>
-  
+  <div class="container d-flex flex-wrap gap-3">
+    <?php foreach($movies as $movie): ?>
+      <div class="card" style="width: 18rem;">
+        <img src="img/<?php echo $movie->image ?>" class="card-img-top" alt="No img">
+        <div class="card-body">
+          <h5 class="card-title"><?php echo $movie->title ?></h5>
+          <p class="card-text"><?php echo $movie->description ?></p>
+          <p class="card-text"><?php echo $movie->price ?></p>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
 </body>
 </html>
